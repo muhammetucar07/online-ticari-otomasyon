e404257407124a9609f757e44ce936e3b51dff90
ucarrr <ucaaar07@gmail.com>
2024-06-22 15:43:24 +0300

    project files deleted

--- online-ticari-otomasyon/Models/ErrorViewModel.fs
+++
@@ -1,9 +0,0 @@
-namespace online_ticari_otomasyon.Models
-
-open System
-
-type ErrorViewModel =
-    { RequestId: string }
-
-    member this.ShowRequestId =
-        not (String.IsNullOrEmpty(this.RequestId))
